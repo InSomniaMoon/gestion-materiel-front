20a11c9394ca974bd583407b2974327be63b6204
Pierre Leroyer <pierre.leroyer69@gmail.com>
2025-09-26 15:42:08 +0200

    feat(my-structure): add duplicate structure name display in component template

--- src/app/pages/admin/my-structure/my-structure.component.ts
+++ src/app/pages/admin/my-structure/my-structure.component.ts
@@ -14,6 +14,7 @@ import { StructureDetailsComponent } from './structure-details/structure-details
   selector: 'app-my-structure',
   imports: [ChildrenStructuresListComponent, StructureDetailsComponent],
   template: `<h1>{{ structure()?.nom_structure }}</h1>
+    <h2>{{ structure()?.nom_structure }}</h2>
     @if (structure()) {
       <app-structure-details [structure]="structure()!" />
     }
